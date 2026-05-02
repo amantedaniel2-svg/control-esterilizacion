46f674329076615afa8a5c4f381abb00d336d67f
amantedaniel2-svg <amantedaniel2@gmail.com>
2026-05-02 12:18:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
-const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 app.use(cors());
@@ -10,53 +9,34 @@ app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: "uploads/" });
 
-// 🔐 PEGAR TUS DATOS DE SUPABASE ACÁ
-const supabase = createClient(
-https://xvzqypytfxvroedqxfbw.supabase.co/rest/v1/
-  sb_publishable_4ElkPdrxsNCeMVTi7woAIA_DEbbi4LX
-);
+let registros = [];
 
 // FRONTEND
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// ================= OPA =================
-app.post("/opa", upload.single("foto"), async (req, res) => {
+// OPA
+app.post("/opa", upload.single("foto"), (req, res) => {
   const { tecnico, ppm } = req.body;
 
   const estado = ppm >= 0.3 ? "ACEPTADO" : "RECHAZADO";
 
-  await supabase.from("registros").insert({
+  registros.push({
     tecnico,
     tipo: "OPA",
     resultado: ppm,
-    estado,
-    fecha: new Date()
+    estado
   });
 
   res.redirect("/");
 });
 
-// ================= ATP =================
-app.post("/atp", upload.single("foto"), async (req, res) => {
-  const { tecnico, resultado, ubicacion } = req.body;
-
-  await supabase.from("registros").insert({
-    tecnico,
-    tipo: "ATP",
-    resultado,
-    ubicacion,
-    fecha: new Date()
-  });
-
-  res.redirect("/");
+// REPORTE
+app.get("/reporte", (req, res) => {
+  res.json(registros);
 });
 
-// ================= REPORTE =================
-app.get("/reporte", async (req, res) => {
-  const { data } = await supabase.from("registros").select("*");
-  res.json(data);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App funcionando");
 });
-
-app.listen(process.env.PORT || 3000);
