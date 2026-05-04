418c1a7da2048cddd6e93757a4cfb085c15234f1
amantedaniel2-svg <amantedaniel2@gmail.com>
2026-05-03 22:14:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 app.use(cors());
@@ -9,7 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: "uploads/" });
 
-let registros = [];
+// 🔐 PONÉ TUS DATOS REALES
+const supabase = createClient(
+  "https://xvzqypytfxvroedqxfbw.supabase.co",
+  "sb_publishable_4ElkPdrxsNCeMVTi7woAIA_DEbbi4LX"
+);
 
 // FRONTEND
 app.get("/", (req, res) => {
@@ -17,26 +22,37 @@ app.get("/", (req, res) => {
 });
 
 // OPA
-app.post("/opa", upload.single("foto"), (req, res) => {
-  const { tecnico, ppm } = req.body;
+app.post("/opa", upload.single("foto"), async (req, res) => {
+  try {
+    const { tecnico, ppm } = req.body;
 
-  const estado = ppm >= 0.3 ? "ACEPTADO" : "RECHAZADO";
+    const estado = ppm >= 0.3 ? "ACEPTADO" : "RECHAZADO";
 
-  registros.push({
-    tecnico,
-    tipo: "OPA",
-    resultado: ppm,
-    estado
-  });
+    await supabase.from("registros").insert({
+      tecnico,
+      tipo: "OPA",
+      resultado: ppm,
+      estado
+    });
 
-  res.redirect("/");
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.send("Error guardando datos");
+  }
 });
 
 // REPORTE
-app.get("/reporte", (req, res) => {
-  res.json(registros);
+app.get("/reporte", async (req, res) => {
+  try {
+    const { data } = await supabase.from("registros").select("*");
+    res.json(data);
+  } catch (error) {
+    res.send("Error obteniendo datos");
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App funcionando");
+});
 });
